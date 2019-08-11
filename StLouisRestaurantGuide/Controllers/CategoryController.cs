eb01bd0f528e66708f971e83518b576790cc7da0
Marie Cowen <marie.cowen.87@gmail.com>
2019-08-10 19:43:54 -0500

    Added controllers, models, viewmodels, and views.

---
+++ StLouisRestaurantGuide/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StLouisRestaurantGuide.Data;
+using StLouisRestaurantGuide.Models;
+using StLouisRestaurantGuide.ViewModels.Categories;
+
+namespace StLouisRestaurantGuide.Controllers
+{
+    public class CategoryController
+    {
+        private ApplicationDbContext context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        //public IActionResult Index()
+        //{
+
+            //List<CategoryListItemViewModel> categories = CategoryListItemViewModel.GetCategories(context);
+            //return View(categories);
+
+        //}
+    }
+}
---
+++ StLouisRestaurantGuide/Models/CategoryRestaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StLouisRestaurantGuide.Models
+{
+    public class CategoryRestaurant
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int LocationId { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual Restaurant Restaurant { get; set; }
+
+    }
+}
